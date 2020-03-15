e95a00c9d580d2203b05f201342c7ddf1fdd51ce
rozaqkurnia <gazerockrozaq@gmail.com>
2020-03-15 18:09:12 +0700

    Initial commit created spring boot app

---
+++ gdtokobuku/src/main/java/io/gedang/gdtokobuku/TokoBukuOnlineApplication.java
@@ -0,0 +1,13 @@
+package io.gedang.gdtokobuku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokoBukuOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokoBukuOnlineApplication.class, args);
+	}
+
+}
---
+++ gdtokobuku/src/test/java/io/gedang/gdtokobuku/TokoBukuOnlineApplicationTests.java
@@ -0,0 +1,16 @@
+package io.gedang.gdtokobuku;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TokoBukuOnlineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
